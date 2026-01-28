683ea0bcf4c1a7153358226ce341ed7d66a5ddb5
Helmanz <helcze4@gmail.com>
2026-01-28 09:56:50 +0100

    Added getting of device location via sensor

--- ViewModel/MainPageViewModel.cs
+++ ViewModel/MainPageViewModel.cs
@@ -1,10 +1,77 @@
-﻿using GeoMemo.Models;
-using GeoMemo.Services;
-using System.Collections.ObjectModel;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.Maui.Devices.Sensors;
 
 namespace GeoMemo.ViewModels;
 
-public class MainPageViewModel
+public class MainPageViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private Location _location;
+    private string _status = "Zjišťuji polohu…";
+
+    public double? Latitude => _location?.Latitude;
+    public double? Longitude => _location?.Longitude;
+    public double? Accuracy => _location?.Accuracy;
+
+    public string Status
+    {
+        get => _status;
+        set
+        {
+            _status = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public async Task LoadAsync()
+    {
+        await GetLocationAsync();
+    }
+
+    private async Task GetLocationAsync()
+    {
+        var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+        if (status != PermissionStatus.Granted)
+        {
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+        }
+
+
+        try
+        {
+            var request = new GeolocationRequest(
+                GeolocationAccuracy.Medium,
+                TimeSpan.FromSeconds(10)
+            );
+
+            _location = await Geolocation.GetLocationAsync(request);
+
+            if (_location == null)
+            {
+                Status = "Polohu se nepodařilo zjistit.";
+                return;
+            }
+
+            Status = "Aktuální poloha:";
+            OnPropertyChanged(nameof(Latitude));
+            OnPropertyChanged(nameof(Longitude));
+            OnPropertyChanged(nameof(Accuracy));
+        }
+        catch (Exception ex)
+        {
+            Status = "Chyba: " + ex.Message;
+        }
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
+
+
+/*public class MainPageViewModel
 {
     private readonly SQLiteManager _db;
 
@@ -25,4 +92,4 @@ public class MainPageViewModel
             Obce.Add(obec);
     }
 
-}
+}*/
